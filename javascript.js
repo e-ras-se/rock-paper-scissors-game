84930b16fb09c314b13203098ccb3ab9672876bd
e-ras-se <257583692+e-ras-se@users.noreply.github.com>
2026-09-10 16:04:24 -0500

    Add Logic to play entire game 5 rounds

--- javascript.js
+++ javascript.js
@@ -1,75 +1,77 @@
-function getComputerChoice(){
-    choice = Math.floor(Math.random() * 3);
+function playGame(){
+    var humanScore;
+    var computerScore;
 
+    humanScore = 0;
+    computerScore = 0;
 
-    if (choice == 0)
-    {
-        return "rock"
-    }
-    else if (choice == 1)
-    {
-        return "paper"
-    }
-    else
-    {
-        return "scissors"
-    }
-}
+    function playRound(humanChoice, computerChoice){
 
-function getHumanChoice(){
+        humanChoice = humanChoice.toLowerCase();
 
-    return userChoice = prompt("What is your choice: Rock, Paper, or Scissors!");
-}
-
-var humanScore;
-var computerScore;
-
-humanScore = 0;
-computerScore = 0;
-
-function playRound(humanChoice, computerChoice){
-
-    humanChoice = humanChoice.toLowerCase();
-
-    console.log(humanChoice);
-
-    if( humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors" )
-    {
-        if(humanChoice == computerChoice){
-            console.log("Draw! No points awarded.");
-        }
-        else
+        if( humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors" )
         {
-            if( (humanChoice == "rock") && (computerChoice == "scissors") || (humanChoice == "scissors") && (computerChoice == "paper") || (humanChoice == "paper") && (computerChoice == "rock") )
-            {
-                console.log("You win! " + userChoice + " beats " + computerChoice);
-                humanScore++;
+            if(humanChoice == computerChoice){
+                console.log("Draw! No points awarded. | You both chose " + computerChoice);
             }
             else
             {
-                console.log("You lose! " + computerChoice + " beats " + userChoice);
-                computerScore++;
+                if( (humanChoice == "rock") && (computerChoice == "scissors") || (humanChoice == "scissors") && (computerChoice == "paper") || (humanChoice == "paper") && (computerChoice == "rock") )
+                {
+                    console.log("Computer chose: " + computerChoice + ". " + "You win! " + userChoice + " beats " + computerChoice);
+                    humanScore++;
+                }
+                else
+                {
+                    console.log("Computer chose: " + computerChoice + ". " + "You lose! " + computerChoice + " beats " + userChoice);
+                    computerScore++;
+                }
             }
         }
-    }
-    else
-    {
-        return alert("Not a valid choice. Must be some varation\nof rock, paper, scissors.");
+        else
+        {
+            return alert("Not a valid choice. Must be some varation\nof rock, paper, scissors.");
+        }
+
     }
 
+    var i;
+
+    for(i = 0; i < 5; i++){
+
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();  
+
+        function getComputerChoice(){
+            choice = Math.floor(Math.random() * 3);
+
+
+            if (choice == 0)
+            {
+                return "rock"
+            }
+            else if (choice == 1)
+            {
+                return "paper"
+            }
+            else
+            {
+                return "scissors"
+            }
+        }
+
+        function getHumanChoice(){
+
+            return userChoice = prompt("What is your choice: Rock, Paper, or Scissors!");
+        }
+        playRound(humanSelection, computerSelection);
+    }
+
+    console.log("The Final score\n" + "-----------\n" + "Computer: " + computerScore + "\n" + "User: " + humanScore);
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+playGame();
 
-console.log(humanSelection);
-console.log(computerSelection);
-
-
-playRound(humanSelection, computerSelection);
-
-console.log(humanScore);
-console.log(computerScore);
 
 
 
